e5ac1633b1bdbcdb4856fec4e153f7710ea1ee60
Jonathan Ray <jonathanjudsonray@gmail.com>
2023-03-20 13:08:35 -0400

    Attempting to create a navbar

---
+++ client/src/components/Navbar/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+export default function Navbar() {
+  return (
+    <div>
+    <Navbar>
+      <div>
+        <p>My Medi</p>
+      </div>
+    </Navbar>
+    </div>
+  );
+}
+
